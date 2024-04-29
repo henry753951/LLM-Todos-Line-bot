a0c4047fd350f93897640090b7e2cfb2c18fcb27
henry753951 <henryboy753951@gmail.com>
2024-04-29 08:51:56 +0800

    Refactor linebot.py to add Dify class for chat functionality

--- api/linebot.py
+++ api/linebot.py
@@ -13,15 +13,44 @@ from linebot.v3.messaging import (
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
 import dotenv
 import os
-import datetime as dt
 import requests
+import datetime
+import time
 
 # 如果當前目錄有 .env 檔案，則優先使用 .env 檔案中的環境變數
 if ".env" in os.listdir():
     dotenv.load_dotenv()
 
-_access_token = os.environ.get('access_token')
-_channel_secret = os.environ.get('channel_secret')
+
+class Dify:
+    lastCompletion = datetime.datetime.now()
+
+    def __init__(self):
+        api_key = os.environ.get("dify_api_key", "")
+        self.api = os.environ.get("dify_api", "")
+        self.headers = {"Authorization": F"Bearer {api_key}", "Content-Type": "application/json"}
+
+    def chat(self, user_id: str, text: str, conversation_id: str = None):
+        # Microsoft Azure API 預設限制
+        while (datetime.datetime.now() - self.lastCompletion).total_seconds() < 10:
+            time.sleep(1)
+
+        url = F"{self.api}/v1/chat-messages"
+        data = {"inputs": {}, "query": text, "user": user_id, "response_mode": "blocking"}
+        if conversation_id:
+            data["conversation_id"] = conversation_id
+        self.lastCompletion = datetime.datetime.now()
+        response = requests.post(url, headers=self.headers, json=data)
+        return {
+            "conversation_id": str(response.json().get("conversation_id")),
+            "reply": str(response.json().get("answer")),
+        }
+
+
+dify = Dify()
+
+_access_token = os.environ.get("access_token")
+_channel_secret = os.environ.get("channel_secret")
 
 # 建立一個新的藍圖
 route = Blueprint(name="__linebot", import_name=__name__)
@@ -32,14 +61,14 @@ line_handler = WebhookHandler(_channel_secret)
 
 
 # 建立一個使用者字典，用於儲存conversation id
-users = defaultdict(lambda: {'conversation': None})
+users = defaultdict(lambda: {"conversation": None})
 
 
 # 定義一個路由，用於接收 Line Bot 的訊息
-@route.route("/", methods=['POST'])
+@route.route("/", methods=["POST"])
 def chat_callback():
     # 取得 Line Bot 的認證資訊
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers["X-Line-Signature"]
 
     # 取得請求的內容
     body = request.get_data(as_text=True)
@@ -55,22 +84,28 @@ def chat_callback():
         )
         abort(400)
 
-    return 'OK'
+    return "OK"
 
 
 # 定義一個處理訊息的函數
 @line_handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event: MessageEvent):
     with ApiClient(configuration) as api_client:
-        current_app.logger.debug("User:" + event.source.to_dict()['userId'])
+        current_app.logger.debug("User:" + event.source.to_dict()["userId"])
         # 取得使用者的對話
-        conversation = users[event.source.to_dict()['userId']]['conversation']
-
-        reply = ""
+        if "reset" == event.message.text:
+            users[event.source.to_dict()["userId"]]["conversation"] = None
+            reply = "對話已重置"
+        else:
+            response = dify.chat(
+                user_id=event.source.to_dict()["userId"],
+                text=event.message.text,
+                conversation_id=users[event.source.to_dict()["userId"]]["conversation"],
+            )
+            users[event.source.to_dict()["userId"]]["conversation"] = response["conversation_id"]
+            reply = response["reply"]
         # 使用 Line Bot API 回覆訊息
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token, messages=[TextMessage(text=str(reply))]
-            )
+            ReplyMessageRequest(reply_token=event.reply_token, messages=[TextMessage(text=reply)])
         )
